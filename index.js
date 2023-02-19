46ffc15446497123f5b2234e2bf0d91e891ffe52
sergenast <nastia0709@mail.ru>
2023-02-19 19:30:26 +0300

    added html and js code and readme file

---
+++ index.js
@@ -0,0 +1,17 @@
+function showMessage() {
+    console.log('Я учу JavaScript');
+}
+showMessage();
+
+let img1 = document.getElementById('img1');
+
+
+function getPic1() {
+    img1.src = 'https://static9.tgstat.ru/channels/_0/6c/6cbe3d27493df4672e090aecd6a9842f.jpg';
+    img1.alt = 'cat';
+}
+
+function getPic2() {
+    img1.src = 'https://kartinkof.club/uploads/posts/2022-03/1648612809_1-kartinkof-club-p-smeshnie-kartinki-koti-1.jpg';
+    img2.alt = 'cat';
+}
